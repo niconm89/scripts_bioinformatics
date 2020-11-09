4ea7bfda6439f141882b884d4bc7e58a5d5bf5d8
nmoreyra <niconm89@gmail.com>
2020-11-09 12:08:46 -0300

    fixing a bug in convertion mode --reverse in fasta2phylip.py

--- python_utils/fasta2phylip.py
+++ python_utils/fasta2phylip.py
@@ -11,26 +11,19 @@ def convert_fasta2phylip(infile, outfile, mode, aln_type, aln_format):
 	dict_seqs = {}
 	with open(infile,'rt') as inputfile:
 		FASTA = inputfile.readlines()
-		if FASTA[0][0] != '>':
-			print("Input file does not have fasta format.\nLeaving...")
-			exit(1)
+		is_fasta(FASTA[0]) #check if the first line of the file starts with '>', otherwise it will exit
 		for line in FASTA:
 			line = line.rstrip() #let's discard the newline at the end (if any)
-			#now we need to distinguish header from sequence
 			if line[0] == '>': #identify the id of each sequence
 				name = check_nameseq(mode,line[1:]) #avoid ">"
 				dict_seqs[name] = ''
 			else:
 				dict_seqs[name] = dict_seqs[name] + line
-	if check_len(dict_seqs):
-		if check_alphabet(dict_seqs, aln_type):
-			name_seqs = list(dict_seqs.keys())
-			nseqs = len(name_seqs)
-			len_aln = len(dict_seqs[name_seqs[0]])
-			header = do_header(nseqs, len_aln)
+	if check_len(dict_seqs): #check the if all sequences in fasta have the same length
+		if check_alphabet(dict_seqs, aln_type): #check ilegal characters
+			header = do_header(dict_seqs)
 			do_alignment(dict_seqs, mode, aln_format, header, outfile)
-			
-		else:#sequences have not allowed characters
+		else:#sequences have ilegal characters
 			print("Leaving...\n")
 			exit(1)		
 	else:#sequences have different lengths
@@ -38,7 +31,13 @@ def convert_fasta2phylip(infile, outfile, mode, aln_type, aln_format):
 		exit(1)
 #end convert_fasta2phylip
 
-def convert_phylip2fasta(infile, outfile, mode, nogaps = False):
+def is_fasta(first_line_file):
+	'Receive the first line of a file to check if starst with the > symbol.'
+	if first_line_file[0] != '>':
+		print("Input file does not have fasta format.\nLeaving...")
+		exit(1)
+
+def convert_phylip2fasta(infile, outfile, nogaps = False):
 	with open(outfile, 'wt') as OUT:
 		seqs_dict = seqs_from_phylip(infile)
 		for record in seqs_dict.items():
@@ -59,42 +58,53 @@ def seqs_from_phylip(infile):
 		if PHYLIP[0].strip() == '': #control por empty line between header and body
 			PHYLIP.pop(0)
 		if nlines == nseqs: #sequential format
-			for line in PHYLIP:
-				line = line.strip()
-				if line != '':
-					taxon_id = line[:-nchar].rstrip()
-					taxon_seq = line[-nchars:]
-					dict_seqs[taxon_id] = taxon_seq
+			taxon_id,taxon_seq = read_phylip_sequential(PHYLIP)
+			dict_seqs[taxon_id] = taxon_seq
 		else: #interleaved format
-			seqs_order = {}
-			i = 0
-			while i < nseqs: #get the first part (with id) of each taxon
-				i += 1
-				line = PHYLIP.pop(0).strip() #remove this line from file
-				seqs_order[i] = line
-				dict_seqs[i] = ''
-			#Read the rest of the lines with only sequences
-			count = 0
-			for line in PHYLIP: #get the rest of the lines for each taxon
-				line = line.rstrip()
-				if line == '':
-					continue
-				else:
-					taxon_seq = line.replace(' ','')
-					count += 1
-					dict_seqs[count] += taxon_seq
-					if count == nseqs:
-						count = 0
-			acumulate_len = len(dict_seqs[1])
-			#back to the first lines
-			rest_len = nchars - acumulate_len #number of characters in each first line
-			for i in range(1,nseqs+1):
-				first_part_taxon = seqs_order[i].replace(' ','')
-				taxon_id = first_part_taxon[:-rest_len]
-				dict_seqs[i] = first_part_taxon[-rest_len:] + dict_seqs[i]
-				dict_seqs[taxon_id] = dict_seqs.pop(i) #remove key = i and update key taxon_id			
+			dict_seqs.update(read_phylip_interleaved(PHYLIP,nseqs,nchars))			
 	return dict_seqs
 #End seqs_from_phylip
+
+def read_phylip_sequential(PHYLIP_FILE):
+	taxon_id = ''
+	taxon_seq = ''
+	for line in PHYLIP_FILE:
+		line = line.strip()
+		if line != '':
+			taxon_id = line[:-nchar].rstrip()
+			taxon_seq = line[-nchars:]
+	return taxon_id,taxon_seq
+
+def read_phylip_interleaved(PHYLIP_FILE,nseqs,nchars):
+	dict_seqs = {}
+	seqs_order = {}
+	i = 0
+	while i < nseqs: #get the first part (with id) of each taxon
+		i += 1
+		line = PHYLIP_FILE.pop(0).strip() #remove this line from file
+		seqs_order[i] = line
+		dict_seqs[i] = ''
+	#Read the rest of the lines with only sequences
+	count = 0
+	for line in PHYLIP_FILE: #get the rest of the lines for each taxon
+		line = line.rstrip()
+		if line == '':
+			continue
+		else:
+			taxon_seq = line.replace(' ','')
+			count += 1
+			dict_seqs[count] += taxon_seq
+			if count == nseqs:
+				count = 0
+	acumulate_len = len(dict_seqs[1])
+	#back to the first lines
+	rest_len = nchars - acumulate_len #number of characters in each first line
+	for i in range(1,nseqs+1):
+		first_part_taxon = seqs_order[i].replace(' ','')
+		taxon_id = first_part_taxon[:-rest_len]
+		dict_seqs[i] = first_part_taxon[-rest_len:] + dict_seqs[i]
+		dict_seqs[taxon_id] = dict_seqs.pop(i) #remove key = i and update key taxon_id
+	return dict_seqs
 
 def do_alignment(seqs_dict, format_mode, format_aln, header, outfile):
 	with open(outfile, 'wt') as OUT:
@@ -103,30 +113,19 @@ def do_alignment(seqs_dict, format_mode, format_aln, header, outfile):
 		if format_aln == "sequential":
 			for record in seqs_dict.items():
 				seq = spaces_in_sequence(record[1],format_mode)
-				id_seq = record[0]
-				id_len = len(id_seq)
-				line_aln = ''
-				if format_mode == 'strict':
-					nspaces = 10 - id_len #at this position id_len <= 10
-					line_aln = id_seq + ' '*nspaces + seq
-				else:
-					line_aln = id_seq + ' ' + seq
+				idseq = record[0]
+				idseq_spaces = spaces_in_id(idseq,format_mode)
+				line_aln = idseq_spaces + seq
 				OUT.write(line_aln+"\n")
 		else: #alignment format = interleaved
 			id_seqs = list(seqs_dict.keys())
 			lenseq = int(header.split(' ')[1])
 			max_idlen = max([ len(idseq) for idseq in id_seqs ]) #max ID len
 			for idseq in id_seqs:
-				id_len = len(idseq)
 				seq = seqs_dict[idseq][0:20]
 				seq_region = spaces_in_sequence(seq,format_mode)
-				firs_line_seq = ''
-				if format_mode == 'strict':
-					nspaces = 10 - id_len #at this position id_len <= 10
-					first_line_seq = idseq + ' '*nspaces + seq_region
-				else: #format_mode = 'relaxed'
-					nspaces = max_idlen - id_len
-					first_line_seq = idseq + ' ' + seq_region
+				idseq_spaces = spaces_in_id(idseq,format_mode)
+				first_line_seq = idseq_spaces + seq_region
 				OUT.write(first_line_seq+"\n")		
 			OUT.write("\n")			
 			i = 0
@@ -142,16 +141,29 @@ def do_alignment(seqs_dict, format_mode, format_aln, header, outfile):
 				OUT.write("\n")			
 #end do_alignment
 
+def spaces_in_id(idseq, format_mode):
+	id_len = len(idseq)
+	idseq_spaces = ''
+	nspaces = 1
+	if format_mode == 'strict':
+		nspaces = 10 - id_len
+	idseq_spaces = idseq + ' '*nspaces
+	return idseq_spaces
+
 def spaces_in_sequence(sequence,format_mode):
 	aux_seq = ''
 	if format_mode == 'strict':
 		for i in range(0,len(sequence),10):
 			aux_seq += sequence[i:i+10] + ' '
+		aux_seq = aux_seq[:-1]
 	else:
 		aux_seq = sequence
-	return	aux_seq[:-1]	
+	return	aux_seq
 
-def do_header(nseqs, nchars):
+def do_header(dict_seqs):
+	name_seqs = list(dict_seqs.keys())
+	nseqs = len(name_seqs)
+	nchars = len(dict_seqs[name_seqs[0]])
 	return str(nseqs) + ' ' + str(nchars)
 
 def check_alphabet(seqs_dict, aln_type):
@@ -182,12 +194,9 @@ def check_len(seqs_dict):
 				correct = False
 				print("Sequences must have the same length.")
 				break
-	return correct		
+	return correct	
 
 def check_nameseq(mode,id_seq):
-	if '\n' in id_seq:
-		print("Newline character not allowed in sequence ID.\nLeaving...")
-		exit(1)
 	if mode == "strict":
 		if len(id_seq) > 10:
 			id_seq = id_seq[0:10]
@@ -195,13 +204,10 @@ def check_nameseq(mode,id_seq):
 	return id_seq
 
 def check_files(infile,outfile):
-	#infile
 	if not os.path.exists(infile):
 		print("Error: Input file does not exists! " + infile)
 		exit(1)
-	#outfile
 	if outfile != 'convertion.phylip':
-		#cwd = os.getcwd() + '/' #Get the current working directory and add slash (/)
 		if '/' in outfile:
 			elements = outfile.split('/')
 			path_to_dir = '/'.join(elements[:-1])
@@ -220,7 +226,7 @@ def usage():
 	parser.add_argument('--mode', type=str, required=False, choices=['strict', 'relaxed'], default='strict', help='Convertion mode. In strict mode, id sequence must be up to 10, otherwise it will be truncated. [default: strict]')
 	parser.add_argument('--format', type=str, required=False, choices=['sequential', 'interleaved'], default='sequential', help='Alignment format. [default: sequential]')
 	parser.add_argument('--reverse', action='store_true', required=False, help='phylip2fasta mode. The program will expect a phylip format file to convert to a fasta file. If not indicated (--mode), the input file is assumed to have a strict phylip format. If taxon name has a space, e.g. H. sapiens, it will be remove like H.sapiens.')
-	parser.add_argument('--nogaps', action='store_true', required=False, help='Filter out gaps from the alignment. If declare, is mandatory to state the reverse mode (--reverse).')
+	parser.add_argument('--nogaps', action='store_true', required=False, default=False, help='Filter out gaps from the alignment. If declare, is mandatory to state the reverse mode (--reverse).')
 
 	args=parser.parse_args()
 	
@@ -235,10 +241,7 @@ def main():
 			exit(1)		
 		convert_fasta2phylip(args.infile,args.outfile,args.alntype,args.mode,args.format)
 	else:
-		if args.nogaps:
-			convert_phylip2fasta(args.infile,args.outfile,args.mode, True)
-		else:
-			convert_phylip2fasta(args.infile,args.outfile,args.mode)
+		convert_phylip2fasta(args.infile,args.outfile, args.nogaps)
 #end of main function
 
 #%% Main program
